1f652b49c2d21ef504d1f8f326a4b126314721fc
enkidu <inju.cwx@gmail.com>
2026-01-06 11:28:46 +0700

    fix peek-a-slot

--- src/basic/peek-a-slot/Setup.sol
+++ src/basic/peek-a-slot/Setup.sol
@@ -5,24 +5,19 @@ contract Setup{
     bytes32 private hiderOne;
     bytes32[] private hiderTwo;
     bytes32[][] private hiderThree;
-    bytes32 private validHiderOne;
-    bytes32 private validHiderTwo;
-    bytes32 private validHiderThree;
     bool public allHidersFound;
 
     constructor(bytes32 _hider, bytes32[] memory _hiderTwo, bytes32[][] memory _hiderThree) {
-        hiderOne = validHiderOne = _hider;
+        hiderOne = _hider;
         hiderTwo =  _hiderTwo;
-        validHiderTwo = hiderTwo[4];
         hiderThree = _hiderThree;
-        validHiderThree = hiderThree[2][3];
     }
 
     function foundHider(bytes32 _hiderOne, bytes32 _hiderTwo, bytes32 _hiderThree) public {
         if(
-            (keccak256(abi.encodePacked(validHiderOne)) == keccak256(abi.encodePacked(_hiderOne))) &&
-            (keccak256(abi.encodePacked(validHiderTwo)) == keccak256(abi.encodePacked(_hiderTwo))) &&
-            (keccak256(abi.encodePacked(validHiderThree)) == keccak256(abi.encodePacked(_hiderThree))) 
+            (keccak256(abi.encodePacked(hiderOne)) == keccak256(abi.encodePacked(_hiderOne))) &&
+            (keccak256(abi.encodePacked(hiderTwo[4])) == keccak256(abi.encodePacked(_hiderTwo))) &&
+            (keccak256(abi.encodePacked(hiderThree[2][3])) == keccak256(abi.encodePacked(_hiderThree))) 
         ){
             allHidersFound = true;
         }
